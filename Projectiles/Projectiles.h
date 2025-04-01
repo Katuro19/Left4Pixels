0f2a797313a53a2df6ea8630525197420918e060
komiko44240BB <pl.maurice.fr@gmail.com>
2025-04-01 09:39:49 +0200

    Update Projectiles.h

--- Projectiles/Projectiles.h
+++ Projectiles/Projectiles.h
@@ -12,19 +12,28 @@ private:
     QPointF target;
 public:
     Projectile(QGraphicsItem* parent = nullptr, QString filePath = "", QString entityType = "projectile", int damage = 10, bool isBreakble = true, int peirces = 0,int bounces = 0, int HP = 50, QPointF target = {0,0});
+
     ~Projectile();
+
     void setDamage(int damage);
-    void setBreakble(bool isBreakble);
-    void setPeirces(int peirces);
-    void setBounces(int bounces);
-    void setHP(int HP);
-    void setTarget(QPointF target);
     int getDamage() const;
+
     bool getBreakble() const;
+    void setBreakble(bool isBreakble);
+
     int getPeirces() const;
+    void setPeirces(int peirces);
+
     int getBounces() const;
+    void setBounces(int bounces);
+
     int getHP() const;
+    void setHP(int HP);
+
     QPointF getTarget() const;
+    void setTarget(QPointF target);
+
+
 };
 
 #endif //PROJECTILES_H
